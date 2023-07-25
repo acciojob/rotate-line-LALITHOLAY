eaf9c9e42a6cf060982e13b3be6ebcdcd432ef61
LALITHOLAY <130987029+LALITHOLAY@users.noreply.github.com>
2023-07-25 18:21:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const line = document.getElementById('line');
+let angle = 45; // Set the initial angle to 45 degrees.
+
+function rotateLine() {
+  angle += 1;
+  line.style.transform = `rotate(${angle}deg)`;
+  requestAnimationFrame(rotateLine);
+}
+
+rotateLine();
